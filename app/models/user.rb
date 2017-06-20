03c133b58d484803d14d3bc139989daa5e31614a
teeyingyap <yapteeying@gmail.com>
2017-06-21 02:35:33 +0800

    added dependent destroy for transactions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :authentications, :dependent => :destroy
   has_many :listings, :dependent => :destroy
   has_many :comments, :dependent => :destroy
-  has_many :transactions
+  has_many :transactions, :dependent => :destroy
   # before_save :create_remember_token
 
   validates :username, 
